05ba52a2a1c6ce640eeaead51f1253736a0b316b
tanjid111 <tanjidhossain@gmail.com>
2022-05-26 02:05:59 -0700

    payment intent created

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -192,7 +194,19 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users)
         })
+        /* ----------------------------------------------------Payment-------------------------------------------- */
+        app.post('/create-payment-intent', async (req, res) => {
+            const product = req.body;
+            const price = product.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
 
+        })
     }
     finally {
 
