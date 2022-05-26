3e590c38603b7dae4bb1e6e0beb750684dc36d1e
tanjid111 <tanjidhossain@gmail.com>
2022-05-26 10:15:37 -0700

    final1

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function run() {
 
 
         /* ----------------------------------------------------Purchases-------------------------------------------- */
-        app.get('/purchase', verifyJWT, verifyAdmin, async (req, res) => {
+        app.get('/purchase', verifyJWT, async (req, res) => {
             const query = {};
             const cursor = purchaseCollection.find(query)
             const result = await cursor.toArray();
@@ -134,15 +134,9 @@ async function run() {
         //Get purchase with user's email id for MyOrder dashboard
         app.get('/purchase', verifyJWT, async (req, res) => {
             const customerEmail = req.query.customerEmail;
-            const decodedEmail = req.decoded.email;
-            if (customerEmail === decodedEmail) {
-                const query = { customerEmail: customerEmail };
-                const purchases = await purchaseCollection.find(query).toArray();
-                return res.send(purchases);
-            }
-            else {
-                return res.status(403).send({ message: 'forbidden access' });
-            }
+            const query = { customerEmail: customerEmail };
+            const purchases = await purchaseCollection.find(query).toArray();
+            return res.send(purchases);
         })
 
         //Delete Purchase from My orders by  user
